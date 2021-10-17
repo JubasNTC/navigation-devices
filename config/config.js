ceef47996db565dab0952ed61ff44283c6fdfb38
JubasNTC <jubasntc@gmail.com>
2021-10-17 15:38:21 +0300

    feat: add support sequelize orm

---
+++ config/config.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_DATABASE_HOST,
+    dialect: "postgres",
+    operatorsAliases: false,
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    operatorsAliases: false,
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    operatorsAliases: false,
+  },
+};
